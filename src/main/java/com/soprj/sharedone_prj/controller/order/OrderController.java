a61ea5de23157e0ae753c5dc0a9606e8a0b9825a
Zzinnn <110443833+Zzinnn@users.noreply.github.com>
2022-12-30 10:12:54 +0900

    commit

--- src/main/java/com/soprj/sharedone_prj/controller/order/OrderController.java
+++ src/main/java/com/soprj/sharedone_prj/controller/order/OrderController.java
@@ -1,9 +1,16 @@
 package com.soprj.sharedone_prj.controller.order;
 
+import com.soprj.sharedone_prj.service.order.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("order")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
 }
