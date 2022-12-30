e8e95b49e1ce4e09979080d122c1473535e65b95
Kimchi.Disaster <ha971206@gmail.com>
2022-12-30 09:32:23 +0900

    기본 양식 수정

--- src/main/java/com/soprj/sharedone_prj/controller/price/PriceController.java
+++ src/main/java/com/soprj/sharedone_prj/controller/price/PriceController.java
@@ -1,4 +1,9 @@
 package com.soprj.sharedone_prj.controller.price;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("price")
 public class PriceController {
 }
--- src/main/java/com/soprj/sharedone_prj/domain/item/ItemDto.java
+++ src/main/java/com/soprj/sharedone_prj/domain/item/ItemDto.java
@@ -1,4 +1,7 @@
 package com.soprj.sharedone_prj.domain.item;
 
+import lombok.Data;
+
+@Data
 public class ItemDto {
 }
