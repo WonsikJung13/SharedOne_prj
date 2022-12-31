38034e40aa65b772a653e82884406a2115a463af
Kimchi.Disaster <ha971206@gmail.com>
2022-12-31 23:37:45 +0900

    Buyer 기능 구현

--- src/main/java/com/soprj/sharedone_prj/mapper/buyer/BuyerMapper.java
+++ src/main/java/com/soprj/sharedone_prj/mapper/buyer/BuyerMapper.java
@@ -1,4 +1,23 @@
 package com.soprj.sharedone_prj.mapper.buyer;
 
+import com.soprj.sharedone_prj.domain.buyer.BuyerDto;
+
+import java.util.List;
+
 public interface BuyerMapper {
+
+    List<BuyerDto> list();
+
+    BuyerDto selectDuo(String m_buyer_name, int m_buyer_id);
+
+    int addBuyer(BuyerDto buyer);
+
+    int insert(BuyerDto buyer);
+
+    int updateBuyer(BuyerDto buyer);
+
+    int removeById(int m_buyer_id);
+
+    BuyerDto select(int m_buyer_id);
+
 }
