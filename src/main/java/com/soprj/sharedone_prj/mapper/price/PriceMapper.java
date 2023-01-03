bdd857857931a5d6396180ec324ed46ae58ba6a6
Limdaram <clara9182@naver.com>
2023-01-03 15:36:36 +0900

    price register db 등록 가능

--- src/main/java/com/soprj/sharedone_prj/mapper/price/PriceMapper.java
+++ src/main/java/com/soprj/sharedone_prj/mapper/price/PriceMapper.java
@@ -1,4 +1,11 @@
 package com.soprj.sharedone_prj.mapper.price;
 
+import com.soprj.sharedone_prj.domain.price.PriceDto;
+
+import java.util.List;
+
 public interface PriceMapper {
+    List<PriceDto> getPriceList();
+
+    int insertRegister(PriceDto price);
 }
