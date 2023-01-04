610558a5c6adec5148163e72fd86a615d6c70aa4
Zzinnn <110443833+Zzinnn@users.noreply.github.com>
2023-01-04 21:02:14 +0900

    commit

--- src/main/java/com/soprj/sharedone_prj/mapper/order/OrderMapper.java
+++ src/main/java/com/soprj/sharedone_prj/mapper/order/OrderMapper.java
@@ -1,4 +1,12 @@
 package com.soprj.sharedone_prj.mapper.order;
 
+import com.soprj.sharedone_prj.domain.buyer.BuyerDto;
+
+import java.util.List;
+
 public interface OrderMapper {
+
+    List<BuyerDto> buyerSelect();
+
+  BuyerDto buyerList(String m_Buyer_Id);
 }
