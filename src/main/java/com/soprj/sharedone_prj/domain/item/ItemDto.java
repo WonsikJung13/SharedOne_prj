0c443965f340caf94f326d1dd4d9350ebb1319ad
gmlrmrtjd <gmlrmrtjd@naver.com>
2023-01-05 10:21:49 +0900

    item_id 문자열 숫자 조합 완료 페이지네이션 진행중

--- src/main/java/com/soprj/sharedone_prj/domain/item/ItemDto.java
+++ src/main/java/com/soprj/sharedone_prj/domain/item/ItemDto.java
@@ -2,11 +2,23 @@ package com.soprj.sharedone_prj.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ItemDto {
-    private int m_item_id;
+    private String m_item_id;
     private String m_item_name;
     private String m_item_unit;
     private String m_item_group;
     private String m_item_manufacturer;
+    private int m_item_number;
+
+    public int lastPageNumber;
+    public int leftPageNumber;
+    public int rightPageNumber;
+    public int currentPageNumber;
+    private boolean hasPrevButton;
+    private boolean hasNextButton;
+    private int jumpPrevPageNumber;
+    private int jumpNextPageNumber;
 }
