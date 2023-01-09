f7d889f2f402082bc9ce1cd9037ff29f2e2812bc
gmlrmrtjd <gmlrmrtjd@naver.com>
2023-01-09 10:56:43 +0900

    다중값 보내기 도전중

--- src/main/java/com/soprj/sharedone_prj/controller/item/ItemController.java
+++ src/main/java/com/soprj/sharedone_prj/controller/item/ItemController.java
@@ -1,14 +1,17 @@
 package com.soprj.sharedone_prj.controller.item;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.soprj.sharedone_prj.domain.item.CartDto;
 import com.soprj.sharedone_prj.domain.item.ItemDto;
 import com.soprj.sharedone_prj.service.item.ItemService;
+import org.apache.tomcat.Jar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("item")
@@ -34,12 +37,45 @@ public class ItemController {
     }
 
     @PostMapping("register")
-    public String register(ItemDto itemDto
-    ) {
-        System.out.println("등록진행!" + itemDto);
-        int cnt = itemService.register(itemDto);
+    @ResponseBody
+    public String register(@RequestBody Map<String, Object> itemDto) throws Exception {
+        System.out.println("???" + itemDto);
+//        for ( String param : paramAray) {
+//            System.out.println("param: " + param);
+//        }
+//        System.out.println("등록진행!" + itemDto);
+//        System.out.println(itemDto.get(0));
+//        System.out.println("name: " + itemDto.get("m_item_id"));
+//        System.out.println(itemDto.get("m_item_group"));
+
+//        int cnt = itemService.register(itemDto);
+
         return "redirect:/item/list";
     }
+
+//    @PostMapping("registerList")
+//    @ResponseBody
+//    public Map<String, Object> registerList(ItemDto itemDto) throws Exception {
+//        System.out.println("ajax 요청 도착!" + itemDto);
+//        Map<String, Object> map = new HashMap<String, Object>();
+//
+//        return map;
+//    }
+
+    @PostMapping("registerList")
+    @ResponseBody
+    public Map<String, Object> registerList (ItemDto itemDto) throws Exception {
+//        String m_item_name, String m_item_group, String m_item_manufacturer, String m_item_unit
+//        ItemDto itemDto = new ItemDto();
+//        itemDto.setM_item_name(m_item_name);
+//        itemDto.setM_item_group(m_item_group);
+//        itemDto.setM_item_manufacturer(m_item_manufacturer);
+//        itemDto.setM_item_unit(m_item_unit);
+        System.out.println(itemDto);
+//        itemDto.setM_item_name(1);
+        return null;
+    }
+
 
     @GetMapping("list")
     public void list(Model model,
@@ -64,4 +100,18 @@ public class ItemController {
 
         return "redirect:/item/list";
     }
+
+//    @PutMapping("itemCart")
+//    @ResponseBody
+//    public Map<String, Object> itemCart(@RequestBody CartDto cartDto, Model model) {
+//        System.out.println("item: " + cartDto);
+//        Map<String, Object> map = new HashMap<>();
+//
+//        itemService.itemCart(cartDto);
+//
+//        model.addAttribute("cartDto", cartDto);
+//        System.out.println("cartDto: " + cartDto);
+////        map.put()
+//        return map;
+//    }
 }
